af6e112bdd42f802e79c5d0b5a1e249cf1e17c9a
mandylyin <mandylyin@sina.com>
2018-01-25 16:48:43 +0800

    First add

---
+++ bloom-upms/bloom-upms-server/src/test/java/com/bloom/upms/UpmsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bloom.upms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UpmsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
